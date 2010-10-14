c678dd5ecff0fc9712335c3885bb3358c8d59c66
Pablo Escobar <pablo@barrabin.org>
2010-10-15 01:22:05 +0200

    added gpl header

--- log2georss.py
+++ log2georss.py
@@ -8,6 +8,20 @@ from PyRSS2Gen import *
 
 #pylint: disable-msg=C0301
 
+
+    #This program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License, or
+    #(at your option) any later version.
+
+    #This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    #GNU General Public License for more details.
+
+    #http://gitorious.org/log2georss
+
+
 def main():
 
     """ main function """
@@ -17,6 +31,7 @@ def main():
 
     # by default I will save file with cached ips in same dir
     # where the script resides
+    #
     cached_ips_file = sys.path[0] + '/' + 'ip_locations.pickle'
 
     rssitemtitle = ''
